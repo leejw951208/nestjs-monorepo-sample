2c30cda0b4018a34af968ab113d66fa445d206f5
leejw951208 <leejw951208@gmail.com>
2025-12-02 00:02:04 +0900

    feat: .

--- apps/user/src/v1/auth/auth.module.ts
+++ apps/user/src/v1/auth/auth.module.ts
@@ -1,14 +1,11 @@
 import { CommonModule } from '@libs/common/common.module'
 import { Module } from '@nestjs/common'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { JwtModule } from '@nestjs/jwt'
-import { PassportModule } from '@nestjs/passport'
 import { UserModule } from 'apps/user/src/v1/user/user.module'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 
 @Module({
-    imports: [PassportModule.register({ defaultStrategy: 'jwt' }), CommonModule, UserModule],
+    imports: [CommonModule, UserModule],
     controllers: [AuthController],
     providers: [AuthService]
 })
--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -3,6 +3,7 @@ import { JwtUtil } from '@libs/common/utils/jwt.util'
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigType } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import commonEnvConfig from './config/env/common-env.config'
 import { JwtAccessStrategy } from './strategy/jwt-access.strategy'
 import { JwtRefreshStrategy } from './strategy/jwt-refresh.strategy'
@@ -10,6 +11,7 @@ import { ClsUtil } from './utils/cls.util'
 
 @Module({
     imports: [
+        PassportModule.register({ defaultStrategy: 'jwt' }),
         JwtModule.registerAsync({
             global: true,
             imports: [ConfigModule],
